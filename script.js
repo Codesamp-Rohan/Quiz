53cf28c368fff2a0924d1b773c57360b324104f9
Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-03-17 21:20:01 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function select1(){
+    document.getElementById("answer1").style.backgroundColor = "#217a347a";
+    const a = document.getElementById("answer").value;
+}
+function select2(){
+    document.getElementById("answer2").style.backgroundColor = "#217a347a";
+    const a = document.getElementById("answer").value;
+}
+function select3(){
+    document.getElementById("answer3").style.backgroundColor = "#217a347a";
+    const a = document.getElementById("answer").value;
+}
+function select4(){
+    document.getElementById("answer4").style.backgroundColor = "#217a347a";
+    const a = document.getElementById("answer").value;
+}
